bb4c4e45f88ae4bf61bd0825576d3b383ee2ac7a
George Gaskell <georgeburn94@gmail.com>
2025-01-06 20:26:28 +0000

    add: template for about

--- about/views.py
+++ about/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import About
+from django.http import HttpResponse
 
 # Create your views here.
+def about_view(request):
+    most_recent_record = About.objects.all().order_by('-updated_on').first()
+    return render(request, 'about/about.html', {'most_recent_record': most_recent_record})
+
+
+    # return render(
+    #     request,
+    #     "blog/about_me.html",
+    #     {"about": about},
+    # )
--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('blog/', views.PostList.as_view(),
     name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    # path('about/', views.post_detail, name='post_detail'),
 ]
 
