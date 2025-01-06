5fb4caf4d3381ae6d6303f159ac8b7d614bf3e48
George Gaskell <georgeburn94@gmail.com>
2025-01-06 18:17:00 +0000

    add __str__ to models.py

--- about/models.py
+++ about/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class About(models.Model):
     title = models.CharField(max_length=100)
     bio = models.TextField()
-    # updated_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title}"
